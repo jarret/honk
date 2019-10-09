2b8b1dd6f28a951796b57801f0e706608dacd347
Jarret Dyrbye <jarret.dyrbye@gmail.com>
2019-10-09 16:38:45 -0600

    AWS stuf start

---
+++ deploy_htdocs.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import time
+
+from lib.aws.s3 import S3BucketDeploy
+from lib.aws.cloudfront import CloudFrontDistribution
+
+
+if not os.path.exists(".git/"):
+    sys.exit("*** Must be executed from base of the git repository.")
+
+
+
+HONK_SOURCE_DIR = "htdocs/"
+HONK_S3_BUCKET = "rarepepemuseum.com"
+HONK_CLOUDFRONT_DISTRIBUTION = "E2OG3NPN80MPK0"
+
+start_time = time.time()
+s3 = S3BucketDeploy(HONK_SOURCE_DIR, HONK_S3_BUCKET)
+s3.put_dir()
+print("")
+#cf = CloudFrontDistribution(HONL_CLOUDFRONT_DISTRIBUTION)
+#cf.invalidate_all()
+
+print("deploy total time: %.4f seconds" % (time.time() - start_time))
