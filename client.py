9b7b9a0bed8694b34e63d91ed4761c16cfd64615
Jarret Dyrbye <jarret.dyrbye@gmail.com>
2019-10-10 11:30:35 -0600

    client code for testing

---
+++ client.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+import sys
+
+from twisted.python import log
+from twisted.internet import reactor, ssl
+
+from autobahn.twisted.websocket import WebSocketClientFactory
+from autobahn.twisted.websocket import WebSocketClientProtocol
+from autobahn.twisted.websocket import connectWS
+
+from twisted.python.modules import getModule
+
+class TlsHonkClientProtocol(WebSocketClientProtocol):
+
+    def sendHello(self):
+        self.sendMessage("Honk!".encode('utf8'))
+
+    def onOpen(self):
+        self.sendHello()
+
+    def onMessage(self, payload, isBinary):
+        if not isBinary:
+            print("Text message received: {}".format(payload.decode('utf8')))
+        reactor.callLater(1, self.sendHello)
+
+HONK_WSS = "wss://localhost:4443"
+#HONK_WSS = "wss://ws.rarepepemuseum.com:443"
+
+if __name__ == '__main__':
+    log.startLogging(sys.stdout)
+
+    factory = WebSocketClientFactory(HONK_WSS)
+    factory.protocol = TlsHonkClientProtocol
+
+    if factory.isSecure:
+        contextFactory = ssl.ClientContextFactory()
+    else:
+        contextFactory = None
+
+    connectWS(factory, contextFactory)
+    reactor.run()
