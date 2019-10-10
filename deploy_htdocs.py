a7f336bb8be8882272b32eb20da42cd58ba589a2
Jarret Dyrbye <jarret.dyrbye@gmail.com>
2019-10-09 21:43:06 -0600

    cloudfront invalidate settings

--- deploy_htdocs.py
+++ deploy_htdocs.py
@@ -15,13 +15,13 @@ if not os.path.exists(".git/"):
 
 HONK_SOURCE_DIR = "htdocs/"
 HONK_S3_BUCKET = "rarepepemuseum.com"
-HONK_CLOUDFRONT_DISTRIBUTION = "E2OG3NPN80MPK0"
+HONK_CLOUDFRONT_DISTRIBUTION = "E33X3LXLI0JEU7"
 
 start_time = time.time()
 s3 = S3BucketDeploy(HONK_SOURCE_DIR, HONK_S3_BUCKET)
 s3.put_dir()
 print("")
-#cf = CloudFrontDistribution(HONL_CLOUDFRONT_DISTRIBUTION)
-#cf.invalidate_all()
+cf = CloudFrontDistribution(HONK_CLOUDFRONT_DISTRIBUTION)
+cf.invalidate_all()
 
 print("deploy total time: %.4f seconds" % (time.time() - start_time))
